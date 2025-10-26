1a9745fedee60d63df85562699c48566af5b5220
Tigerblue77 <37409593+tigerblue77@users.noreply.github.com>
2025-10-26 23:31:39 +0000

    Step 2

---
+++ constants.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Define the interval for printing temperature table header
+readonly TABLE_HEADER_PRINT_INTERVAL=10
+
+readonly CLASSIC_USER_FAN_CONTROL_PROFILE_ID=1
+readonly INTERPOLATED_USER_FAN_CONTROL_PROFILE_ID=2
+readonly DELL_DEFAULT_FAN_CONTROL_PROFILE_ID=3
