5667f1d5d40c9c2058b743ec22330ccf2f152856
Tigerblue77 <37409593+tigerblue77@users.noreply.github.com>
2025-10-26 23:14:41 +0000

    Improved fan speed interpolation check

--- Dell_iDRAC_fan_controller.sh
+++ Dell_iDRAC_fan_controller.sh
@@ -34,16 +34,22 @@ then
   fi
 fi
 
-# Check if HIGH_FAN_SPEED variable is in hexadecimal format. If not, convert it to hexadecimal
-if [[ "$HIGH_FAN_SPEED" == 0x* ]]
+# Check if fan speed interpolation is enabled
+if [ -z "$HIGH_FAN_SPEED" ] || [ -z "$CPU_TEMPERATURE_THRESHOLD_FOR_FAN_SPEED_INTERPOLATION" ]
 then
-  readonly FAN_SPEED_INTERPOLATION_ENABLED=true
-  readonly DECIMAL_HIGH_FAN_SPEED=$(convert_hexadecimal_value_to_decimal "$HIGH_FAN_SPEED")
-  readonly HEXADECIMAL_HIGH_FAN_SPEED="$HIGH_FAN_SPEED"
-else
   readonly FAN_SPEED_INTERPOLATION_ENABLED=false
-  readonly DECIMAL_HIGH_FAN_SPEED="$HIGH_FAN_SPEED"
-  readonly HEXADECIMAL_HIGH_FAN_SPEED=$(convert_decimal_value_to_hexadecimal "$HIGH_FAN_SPEED")
+else
+  readonly FAN_SPEED_INTERPOLATION_ENABLED=true
+
+  # Check if HIGH_FAN_SPEED variable is in hexadecimal format. If not, convert it to hexadecimal
+  if [[ "$HIGH_FAN_SPEED" == 0x* ]]
+  then
+    readonly DECIMAL_HIGH_FAN_SPEED=$(convert_hexadecimal_value_to_decimal "$HIGH_FAN_SPEED")
+    readonly HEXADECIMAL_HIGH_FAN_SPEED="$HIGH_FAN_SPEED"
+  else
+    readonly DECIMAL_HIGH_FAN_SPEED="$HIGH_FAN_SPEED"
+    readonly HEXADECIMAL_HIGH_FAN_SPEED=$(convert_decimal_value_to_hexadecimal "$HIGH_FAN_SPEED")
+  fi
 fi
 
 IDRAC_LOGIN_STRING=$(set_iDRAC_login_string "$IDRAC_HOST" "$IDRAC_USERNAME" "$IDRAC_PASSWORD")
@@ -75,7 +81,7 @@ if $FAN_SPEED_INTERPOLATION_ENABLED
 then
   echo "Fan speed lower value: $DECIMAL_FAN_SPEED%"
   echo "Fan speed higher value: $DECIMAL_HIGH_FAN_SPEED%"
-  echo "CPU lower temperature threshold: $CPU_TEMPERATURE_THRESHOLD_FOR_FAN_SPEED_INTERPOLATION°C"
+  echo "CPU lower temperature threshold: \"$CPU_TEMPERATURE_THRESHOLD_FOR_FAN_SPEED_INTERPOLATION\"°C"
   echo "CPU higher temperature threshold: \"$CPU_TEMPERATURE_THRESHOLD\"°C"
 else
   echo "Fan speed objective: $DECIMAL_FAN_SPEED%"
